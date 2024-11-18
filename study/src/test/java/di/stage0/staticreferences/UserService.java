13f03e8dd42ab129dead1caffd71ea11e4b20dfc
speculatingwook <bwook9908@gmail.com>
2024-11-18 15:25:44 +0900

    feat: complete week0

--- study/src/test/java/di/stage0/staticreferences/UserService.java
+++ study/src/test/java/di/stage0/staticreferences/UserService.java
@@ -3,6 +3,11 @@ package di.stage0.staticreferences;
 import di.User;
 
 class UserService {
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     public static User join(User user) {
         UserDao.insert(user);
